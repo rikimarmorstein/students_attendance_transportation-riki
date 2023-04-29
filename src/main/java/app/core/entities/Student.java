c5792f84491d29d8ee37179524c7a80bbb7491b8
rikimarmorstein <rikim1010@gmail.com>
2023-04-29 23:30:04 +0300

    Initial commit

---
+++ src/main/java/app/core/entities/Student.java
@@ -0,0 +1,41 @@
+package app.core.entities;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "students")
+@ToString(exclude = "teacher")
+@Builder
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private int id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String phone;
+    private int numClass;
+    @Column(nullable = false)
+    private boolean isTravel;
+    private String pickupAddress;
+    private int numBus;
+    @Column(nullable = false)
+    private Hour hour;
+    private String remark;
+    @ManyToOne
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    public  enum  Hour{
+        THIRTEEN, SIXTEEN;
+    }
+}
+
---
+++ src/main/java/app/core/entities/Teacher.java
@@ -0,0 +1,33 @@
+package app.core.entities;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "teachers")
+@ToString(exclude = "students")
+@Builder
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+    private int id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String phone;
+    private int numClass;
+    @Column(nullable = false)
+    private String password;
+    @JsonIgnore
+    @OneToMany(mappedBy = "student")
+    private List<Student> students;
+}

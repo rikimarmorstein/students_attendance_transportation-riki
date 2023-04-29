4931e47fe818452028956a550a1fdd32002c4985
rikimarmorstein <rikim1010@gmail.com>
2023-04-30 00:52:39 +0300

    update

---
+++ src/main/java/app/core/services/TeacherService.java
@@ -0,0 +1,13 @@
+package app.core.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class TeacherService {
+
+
+
+
+}

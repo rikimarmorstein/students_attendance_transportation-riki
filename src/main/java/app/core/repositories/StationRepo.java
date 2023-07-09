ba7b137c85a13116d36d2d1cff3305cb4e42b79d
rikimarmorstein <rikim1010@gmail.com>
2023-07-09 22:48:47 +0300

    successfully

--- src/main/java/app/core/repositories/StationRepo.java
+++ src/main/java/app/core/repositories/StationRepo.java
@@ -1,7 +1,6 @@
 package app.core.repositories;
 
 import app.core.entities.Station;
-import app.core.entities.Transportation;
 import app.core.exception.SystemException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -19,6 +18,7 @@ public interface StationRepo extends JpaRepository<Station, Integer> {
     @Modifying
     @Query(value = "update `stations` set `num_station` =`num_station` -1 where `num_station`>=? and `num_station`<?;", nativeQuery = true)
     void updateNumStationBetweenlehaktin(int numStation) throws SystemException;
+
     @Modifying
     @Query(value = "update `stations` set `num_station` =`num_station` +1 where `num_station`>=? and `num_station`<?;", nativeQuery = true)
     void updateNumStationBetweenlehagdil(int numStation) throws SystemException;
